fe08ccbf5a4763380e2acb5f95fec05a83326f97
Michelle Clement <michellelclement@gmail.com>
2020-09-11 16:37:16 +0000

    Templates file added, app.py updated to link to mongo.db

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
-from flask import Flask
+from flask import Flask, flash, render_template, redirect, request, session, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 # The above will only install if can find the env.py file. Remember this will not be pushed to Github
@@ -7,11 +9,21 @@ if os.path.exists("env.py"):
 # Create an instance of flask in a variable app
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+# Create an instance of PyMongo - app below links to the app defined above
+
+mongo = PyMongo(app)
+
 
 # Check properly configured with a test function
 @app.route("/")
-def hello():
-    return "Hello World.... again"
+@app.route("/get_tasks")
+def get_tasks():
+    tasks = mongo.db.tasks.find()
+    return render_template("tasks.html", tasks=tasks)
 
 
 # how and where to test function 
