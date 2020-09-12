1258dce22f9c545fc4490900679e0c41e806f2a7
Michelle Clement <michellelclement@gmail.com>
2020-09-12 09:12:31 +0000

    Add register template

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, flash, render_template, redirect, request, session, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 # The above will only install if can find the env.py file. Remember this will not be pushed to Github
@@ -25,6 +26,11 @@ def get_tasks():
     tasks = mongo.db.tasks.find()
     return render_template("tasks.html", tasks=tasks)
 
+
+# Register function
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
 
 # how and where to test function 
 if __name__ == "__main__":
