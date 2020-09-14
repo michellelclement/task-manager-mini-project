9aae944a773b4e42a7ac5a63aac22dc74366a291
Michelle Clement <michellelclement@gmail.com>
2020-09-14 14:16:53 +0000

    Edit task added

--- app.py
+++ app.py
@@ -177,6 +177,21 @@ def add_category():
     return render_template("add_category.html")
 
 
+# Function to edit category
+@app.route("/edit_category/<category_id>", methods=["GET", "POST"])
+def edit_category(category_id):
+    if request.method == "POST":
+        submit = {
+            "category_name": request.form.get("category_name")
+        }
+        mongo.db.categories.update({"_id": ObjectId(category_id)}, submit)
+        flash("Category Successfully Updated")
+        return redirect(url_for("get_categories"))
+
+    category = mongo.db.categories.find_one({"_id": ObjectId(category_id)})
+    return render_template("edit_category.html", category=category)
+
+
 # how and where to test function
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
